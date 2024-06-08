2a052a2f94d57e7b6f7996f1468d555b81dc6d8d
Erfan Alishahi <erfanalishahi02@gmail.com>
2024-06-08 16:13:37 +0330

    new file:   2023/day1-part1.py

---
+++ 2023/day1-part1.py
@@ -0,0 +1,23 @@
+def combine(string):
+    digit_list = list()
+    for index, word in enumerate(string):
+        if word.isdigit():
+            digit_list.append((word, index))
+    # sort list by index
+    digit_list = sorted(digit_list, key=lambda ind: ind[1])
+    # return int number
+    if len(digit_list) == 1: # if there is just one digit in string
+        return int(digit_list[0][0] * 2)
+    elif len(digit_list) == 0: # if there is no digit in string
+        return 0
+    else: # if there are many digits in string
+        return int(digit_list[0][0] + digit_list[-1][0])
+    
+# open text file 
+with open(r'C:\\Users\\Lenovo\\Desktop\\test.txt') as file: # Enter text file path into <.txt>
+    text = file.readlines()
+# Combine line by line
+Sum = 0
+for string in text:
+    Sum += combine(string)
+print(Sum)
